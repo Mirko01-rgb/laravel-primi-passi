afddb91f2154cd770bbb75f42fd0bce7e6528267
Mirko <mirkosbaglia2001@gmail.com>
2021-05-14 16:23:50 +0200

    Esercizio terminato

---
+++ MyLaravel2/resources/views/myView.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My view</title>
+    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+  </head>
+
+  <body>
+    <h1>Hello world</h1>
+
+  </body>
+</html>
